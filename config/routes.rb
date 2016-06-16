df5b4ae1c5306a8fa4452bbb3d36005937c6676f
beechan <hisobe@netprotections.co.jp>
2016-06-16 05:48:42 +0000

    add follwings/followers

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   get    'signup', to: 'users#new'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
-  delete'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :users
   resources :sesssions, only: [:new, :create, :destroy]
   resources :microposts
+  resources :relationships, only: [:create, :destroy]
 end
