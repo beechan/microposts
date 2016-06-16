4eb96567f2970d93f5c8f6ee5d6eb76fcb2a1626
beechan <hisobe@netprotections.co.jp>
2016-06-16 01:29:16 +0000

    userプロフィールの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,23 @@ class UsersController < ApplicationController
     end
   end
   
-  private
+  def edit
+    @user = current_user
+  end
+  
+  def update
+	   @user = current_user
+	   @user.update_attributes(user_params)
+	   if @user.save
+	     flash[:success] = "こうしんされました"
+	   redirect_to @user
+	 else
+	   render 'new'
+	 end
+  end
   
   def user_params
     params.require(:user).permit(:name, :email, :password, 
-                                 :password_confirmation)
+                                 :password_confirmation, :area, :user_profile)
   end
 end
