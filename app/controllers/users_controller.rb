d14596c722162b69f28fa695d7591830254aeca5
beechan <hisobe@netprotections.co.jp>
2016-06-17 02:33:43 +0000

    1st commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "さんぷるあぷりのせかいへようこそ"
       redirect_to @user
     else
       render 'new'
     end
+  end
+  
+  def followings
+    @follow = User.find(params[:id]).follower_users
+    @title = "followings"
+    render 'follow'
+  end
+  
+  def followers
+    @follow = User.find(params[:id]).following_users
+    @title = "followers"
+    render 'follow'
   end
   
   private
