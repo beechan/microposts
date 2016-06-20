c2046649002e5de6344d33f5a61b4fec1710039f
beechan <hisobe@netprotections.co.jp>
2016-06-20 03:07:21 +0000

    ページネーションを追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,7 @@ class StaticPagesController < ApplicationController
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
     end
+      @feed_items = current_user.feed_items.includes(:user).page(params[:page]).per(3)
+  
   end
 end
