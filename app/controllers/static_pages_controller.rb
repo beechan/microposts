df91292752bc3af0cce3e5ced1b82e7985ec43d3
beechan <hisobe@netprotections.co.jp>
2016-06-20 04:53:28 +0000

    @feed_itemsのバグとり

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      @items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(3)
     end
-      @feed_items = current_user.feed_items.includes(:user).page(params[:page]).per(3)
+      
   
   end
 end
